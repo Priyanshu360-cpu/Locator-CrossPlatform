aa7d8d3a27951cb80116c088b735eab81a363c47
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-04-29 00:31:55 +0530

    built stack on drawer

--- App.tsx
+++ App.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import  chat from './chatscreen'
-const Drawer = createDrawerNavigator();
-import Home from './homescreen'
+import home from './drawer'
+const Stack = createStackNavigator();
 const App = () => {
   return (
-   
-      <NavigationContainer>
-       <Drawer.Navigator initialRouteName="Home" >
-        <Drawer.Screen name="Home" component={Home} />
-        <Drawer.Screen name="Chatscreen" component={chat} />
-      </Drawer.Navigator>
-      </NavigationContainer>
+    <NavigationContainer independent={true}>
+   <Stack.Navigator >
+        <Stack.Screen
+          name="Home"
+          component={home}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+  </NavigationContainer>
   );
 };
 
