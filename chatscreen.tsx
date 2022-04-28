46e977325e033bb37f90684483f2f0eb30742145
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-04-29 00:09:47 +0530

    started work on chatscreen

---
+++ chatscreen.tsx
@@ -0,0 +1,16 @@
+import {
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    PermissionsAndroid,
+    TouchableOpacity,
+    Linking, 
+    ImageBackground,
+    Platform,
+    View
+  } from 'react-native';
+  import React from 'react';
+import { Component }  from 'react';
+   export default class App extends Component{
+    render(){
+      return (<Text>Under Process</Text>)}};
