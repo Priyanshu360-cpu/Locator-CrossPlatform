f80533405ef04faccdaca6a10d05c734fa04aabd
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-04-29 00:32:11 +0530

    Seperated drwaer files

---
+++ drawer.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import  chat from './chatscreen'
+const Drawer = createDrawerNavigator();
+import Home from './homescreen'
+const App = () => {
+  return (
+   
+      <NavigationContainer independent={true}>
+       <Drawer.Navigator initialRouteName="Home" >
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Chatscreen" component={chat} />
+      </Drawer.Navigator>
+      </NavigationContainer>
+  );
+};
+
+export default App;
